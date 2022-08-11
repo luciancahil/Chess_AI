562d4a570b249254f9cb94bc8b4761769119e72a
royhe <royhe62@yahoo.ca>
2022-08-10 18:43:49 -0700

    finished file capable of isolated games from provided pgn

--- Data Cleaning/src/Main/DataClean.java
+++ Data Cleaning/src/Main/DataClean.java
@@ -1,19 +1,19 @@
 package Main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DataClean {
 
     public static void main(String[] args) {
         BufferedReader reader;
+        String outputPath = "OutputGames.pgn";
+
         try {
             reader = new BufferedReader(new FileReader(
                     "Data/ficsgamesdb_202201_standard2000_nomovetimes_259055.pgn"));
             String line = reader.readLine();
             while (line != null) {
-                System.out.println(line);
+                writeFile(outputPath, line);
                 // read next line
                 line = reader.readLine();
             }
@@ -22,4 +22,23 @@ public class DataClean {
             e.printStackTrace();
         }
     }//Data/ficsgamesdb_202201_standard2000_nomovetimes_259055.pgn
+
+    /**
+     * Writes a line to a file if it is a line representing moves in a chess game. Do nohting otherwise
+     * @param line The line we will write to a file
+     * @param path  The path to the file we will write to
+     */
+    private static void writeFile(String path, String line) throws IOException {
+        if(line.length() == 0 || line.charAt(0) != '1') {
+            return;
+        }
+
+        String addedLine = line.substring(0, line.lastIndexOf('{'));
+
+
+        Writer fileWriter = new FileWriter(path, true);
+        fileWriter.write(addedLine + "\n");
+
+        fileWriter.close();
+    }
 }
