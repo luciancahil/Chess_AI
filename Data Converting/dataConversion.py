4884faec23717fc92a937fc9dbc66aad2b6e5255
royhe <royhe62@yahoo.ca>
2022-08-10 19:32:40 -0700

    added method to turn a board into an input array

--- Data Converting/dataConversion.py
+++ Data Converting/dataConversion.py
@@ -9,6 +9,16 @@ def getBoardString(board):
     return boardString
 
 
-    
+def getInputArray(board):
+    boardString = getBoardString(board)
+    array = [0] * 64
+    valueDict = {"." : 0, "P": 1/12, "p" : 2/12, "N": 3/12, "n": 4/12, "B" : 5/12, "b" : 6/12, "R": 7/12, "r" : 8/12, "Q": 9/12, "q" : 10/12, "K": 11/12, "k" : 12/12}
 
-print(getBoardString(chessBoard))
+    for i in range(64):
+        key = boardString[i]
+        array[i] = valueDict[key]
+    
+    return array
+
+
+print(getInputArray(chessBoard))
