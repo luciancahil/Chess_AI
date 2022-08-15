52b9fc0eb087c23faa078a29128c886f8b26637e
royhe <royhe62@yahoo.ca>
2022-08-15 10:13:27 -0700

    added structure of neural network

--- Engine Training/training.py
+++ Engine Training/training.py
@@ -44,7 +44,7 @@ def getInputArray(board):
     return array
 
 
-
+"""
 
 games = open("Engine Training\\Data\\InputGames.pgn", "r")
 
@@ -87,3 +87,45 @@ for gameLine in gameLines:
 
 train_dataset = TensorDataset(torch.tensor(trainInputs), torch.tensor(trainOutputs))
 test_dataset = TensorDataset(torch.tensor(testInputs), torch.tensor(testOutputs))
+
+"""
+
+
+# Hyper-parameters 
+input_size = 64 # number of squares
+hidden_size = 200 # number of pixels in hidden layer
+num_classes = 230 
+num_epochs = 2 # number of times we go through data
+batch_size = 100
+learning_rate = 0.001
+
+
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size)  # connects the input layer to the first hidden layer
+        self.relu = nn.ReLU() # relu function
+        self.l2 = nn.Linear(hidden_size, hidden_size) # connects the first hidden layer to the second
+        self.l3 = nn.Linear(hidden_size, num_classes) # connects the second hidden layer to the output layer
+
+
+    def forward(self, x):
+        out = self.l1(x) #runs input into hidden layer
+        out = self.relu(out) # runs output of layer 1 through relu
+        out = self.l2(out) # runs relu'd function through to the second hidden layer
+        out = self.relu(out)
+        out = self.l3(out) # runs the relu'd output of the 2nd hidden layer into the output layer
+        return out
+
+
+#no gpu
+device = torch.device('cpu')
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
