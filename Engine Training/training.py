a6850bb168a479c8e73b56fa6a5ae22eeb58ff1b
royhe <royhe62@yahoo.ca>
2022-08-15 09:52:00 -0700

    added CustomTextDataSet class

--- Engine Training/training.py
+++ Engine Training/training.py
@@ -7,3 +7,28 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 import chess
+
+
+class CustomTextDataset(Dataset):
+    def __init__(self, input, labels):
+        self.labels = labels
+        self.input = input
+
+    def __len__(self):
+            return len(self.labels)
+
+    def __getitem__(self, idx):
+            label = self.labels[idx]
+            input = self.input[idx]
+            sample = (input, label)
+            return sample
+
+
+
+
+games = open("Engine Training\\Data\\InputGames.pgn", "r")
+
+gameLines = games.readlines()
+
+
+print(gameLines[0])
